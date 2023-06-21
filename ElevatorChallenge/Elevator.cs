f0c9f2e1215121cd27bc463e20709d1f3b107376
Barasa Ouma <oumabaros@gmail.com>
2023-06-21 04:49:36 +0300

    First Commit

--- ElevatorChallenge/Elevator.cs
+++ ElevatorChallenge/Elevator.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ElevatorChallenge.Elevator;
 
 namespace ElevatorChallenge
 {
     public class Elevator
     {
-        public int[] FloorsServedByElevator { get; set; }
+        private bool[] floorReady;
+        public int CurrentFloor = 1;
+        public int topfloor;
+        public ElevatorState Status = ElevatorState.Idle;
 
-        public int ElevatorCapacity { get; set; }
 
-        public int CurrentFloor { get; set; }
+        public int ElevatorCapacity = 10;
+
+        //public int CurrentFloor { get; set; }
 
         public List<int> PeopleInElevator { get; set; }
-
-        // Priority queue to store floor drop off requests
-        public ElevatorRequestCompare CompareFloorRequests = new ElevatorRequestCompare(0, 0);
-        public PriorityQueue<Person, Person> StopFloorRequest;
+             
 
         public enum ElevatorState
         {
@@ -29,8 +31,12 @@ namespace ElevatorChallenge
 
         public ElevatorState CurrentElevatorState { get; set; }
 
-
-        public Elevator(int[] ElevatorFloorsServedByElevator, int E_Capacity)
+        public Elevator(int NumberOfFloors = 10)
+        {
+            floorReady = new bool[NumberOfFloors + 1];
+            topfloor = NumberOfFloors;
+        }
+        /*public Elevator_(int[] ElevatorFloorsServedByElevator, int E_Capacity)
         {
             FloorsServedByElevator = ElevatorFloorsServedByElevator;
             ElevatorCapacity = E_Capacity;
@@ -39,25 +45,9 @@ namespace ElevatorChallenge
             CurrentElevatorState = ElevatorState.Idle;
             StopFloorRequest = new PriorityQueue<Person, Person>(CompareFloorRequests);
             CompareFloorRequests.FloorsServedByElevator = FloorsServedByElevator.Count();
-        }
+        }*/
 
-        /**
-		  * Floors served by elevator are stored in an array
-		*/
-        public void MoveElevatorToNextFloor(ElevatorState state)
-        {
-            if (state == ElevatorState.Up && CurrentFloor < FloorsServedByElevator[FloorsServedByElevator.Count() - 1])
-            {
-                CurrentFloor = FloorsServedByElevator[Array.IndexOf(FloorsServedByElevator, CurrentFloor) + 1];
-                CompareFloorRequests.CurrentFloor = CurrentFloor;
-            }
-            else if (state == ElevatorState.Down && CurrentFloor > FloorsServedByElevator[0])
-            {
-                CurrentFloor = FloorsServedByElevator[Array.IndexOf(FloorsServedByElevator, CurrentFloor) - 1];
-                CompareFloorRequests.CurrentFloor = CurrentFloor;
-            }
-        }
-
+        
         /**
 		  * Display current people in elevator
 		*/
@@ -72,6 +62,109 @@ namespace ElevatorChallenge
                 CurrentPeopleInElevator.Remove(CurrentPeopleInElevator.Length - 1, 1);
             CurrentPeopleInElevator.Append(")");
             return CurrentPeopleInElevator.ToString();
+        }
+
+        public bool Call(int floor)
+        {
+            if (floor > topfloor||floor<1)
+            {
+                Console.WriteLine("We only have {0} floors. The Floor given is invalid.", topfloor);
+                return false;
+            }
+            else
+            {
+                Status = ElevatorState.Idle;
+                CurrentFloor = floor;
+                floorReady[floor] = false;
+                Console.WriteLine("Elevator being called to {0}", floor);
+                return true;
+            }
+            
+        }
+
+        private void Stop(int floor)
+        {
+            Status = ElevatorState.Idle;
+            CurrentFloor = floor;
+            floorReady[floor] = false;
+            Console.WriteLine("Stopped at floor {0}", floor);
+        }
+
+        private void Descend(int floor)
+        {
+            for (int i = CurrentFloor; i >= 1; i--)
+            {
+                if (floorReady[i])
+                    Stop(floor);
+                else
+                    continue;
+            }
+
+            Status = ElevatorState.Idle;
+            Console.WriteLine("Waiting..");
+        }
+
+        private void Ascend(int floor)
+        {
+            for (int i = CurrentFloor; i <= topfloor; i++)
+            {
+                if (floorReady[i])
+                    Stop(floor);
+                else
+                    continue;
+            }
+
+            Status = ElevatorState.Idle;
+            Console.WriteLine("Waiting..");
+        }
+
+        void StayPut()
+        {
+            Console.WriteLine("That's our current floor");
+        }
+
+        public void FloorPress(int floor)
+        {
+            if (floor > topfloor)
+            {
+                Console.WriteLine("We only have {0} floors", topfloor);
+                return;
+            }
+
+            floorReady[floor] = true;
+
+            switch (Status)
+            {
+
+                case ElevatorState.Down:
+                    Descend(floor);
+                    break;
+
+                case ElevatorState.Idle:
+                    if (CurrentFloor < floor)
+                        Ascend(floor);
+                    else if (CurrentFloor == floor)
+                        StayPut();
+                    else
+                        Descend(floor);
+                    break;
+
+                case ElevatorState.Up:
+                    Ascend(floor);
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+
+        public enum ElevatorStatus
+        {
+            UP,
+            STOPPED,
+            DOWN
         }
     }
 
--- ElevatorChallenge/Program.cs
+++ ElevatorChallenge/Program.cs
@@ -1,1 +1,72 @@
-﻿
+﻿using System;
+using System.Threading;
+
+
+namespace ElevatorChallenge
+{
+    public class Program
+    {
+        private const string QUIT = "q";
+
+        public static void Main(string[] args)
+        {
+        
+        Start:
+            
+            Console.WriteLine("How many floors does the building have?");
+
+            int floor;
+            string floorInput; 
+            Elevator elevator;
+
+            floorInput = Console.ReadLine();
+
+            if (Int32.TryParse(floorInput, out floor))
+                elevator = new Elevator(floor);
+            else
+            {
+                Console.WriteLine("The value entered is not valid!");
+                Console.Beep();
+                Thread.Sleep(2000);
+                Console.Clear();
+                goto Start;
+            }
+
+            string input;
+
+            StartFloor:
+                Console.WriteLine("Which floor are you in?");
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out floor))
+                {
+                    if (!elevator.Call(floor))
+                    {
+                    goto StartFloor;
+                    }
+                }
+                
+                else if (input == QUIT)
+                {
+                    Console.WriteLine("Exiting...");
+                }
+                
+            
+
+            while (input != QUIT)
+            {
+                Console.WriteLine("Which floor are you going to?");
+
+                input = Console.ReadLine();
+                if (Int32.TryParse(input, out floor))
+                    elevator.FloorPress(floor);
+                else if (input == QUIT)
+                    Console.WriteLine("Exiting...");
+                else
+                    Console.WriteLine("The floor you provided is invalid...please try again!");
+            }
+        }
+    }
+
+
+    
+}
